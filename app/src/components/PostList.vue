518b285f3ff65c82e54ed796ee66acdbcccdd8a5
Trottenheimer <ritm.algo@mail.ru>
2023-08-10 17:01:23 +0300

    second commit

---
+++ app/src/components/PostList.vue
@@ -0,0 +1,53 @@
+<template>
+<div v-if="posts.length > 0">
+    <h1>Список постов</h1>
+    <transition-group name="post-list">
+        <post-item
+            v-for="post in posts"
+            :post="post"
+            :key="post.id"
+            @remove="$emit('remove', post)"
+        />
+    </transition-group>  
+</div>
+
+<div
+    v-else
+    style="color: red;"
+>
+    <h1>Список пуст</h1>
+</div>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem"
+export default{
+    components:{
+        PostItem
+    },
+    props:{
+        posts:{
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+<style scoped>
+.post-list-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  .post-list-enter-active,
+  .post-list-leave-active {
+    transition: all 0.3s ease;
+  }
+  .post-list-enter-from,
+  .post-list-leave-to {
+    opacity: 0;
+    transform: translateX(130px);
+  }
+  .post-list-move {
+    transition: transform 0.5s ease;
+  }
+</style>
---
+++ app/src/components/PostPagination.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        {{test}}
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        test
+    }
+}
+</script>
+<style>
+
+</style>
